50d80bd2ec3af03c050fee182b01022279ac7b92
Solehzoda <salikhovajillo@gmail.com>
2024-12-22 12:33:29 +0500

    первый коммит

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        class Animal
+        {
+            public string kind = "DOG";
+            public string name = "BOBIK";
+            public int age = 4;
+            public int weight = 12;
+
+            public void PrintInfo()
+            {
+                Console.WriteLine($"Kind: {kind}");
+                Console.WriteLine($"Name: {name}");
+                Console.WriteLine($"Age: {age}");
+                Console.WriteLine($"Weight: {weight}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            Console.WriteLine($"Kind: {animal.kind}");
+            Console.WriteLine($"Name: {animal.name}");
+            Console.WriteLine($"Age: {animal.age} years");
+            Console.WriteLine($"Weight: {animal.weight} kg");
+        }
+    }
+}
